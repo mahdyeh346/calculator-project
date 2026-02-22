de12178b3e779fe9b568d26c33b07c9f56b8fb9b
mahdyeh <mahdyeh@gmail.com>
2026-02-23 02:13:22 +0330

    add text to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,21 +1,28 @@
 # calculator توابع اصلی ماشین حساب
+
+"""جمع دو عدد"""
 def add(a, b):
     return a + b
 
+"""تفریق دو عدد"""
 def subtract(a, b):
     return a - b
 
+"""ضرب دو عدد"""
 def multiply(a, b):
     return a * b
 
+"""تقسیم دو عدد"""
 def divide(a, b):
     if b == 0:
         return "خطا: تقسیم بر صفر ممکن نیست!"
     return a / b
 
+"""توان: a به توان b"""
 def power(a, b):
     return a ** b
 
+"""باقیمانده"""
 def modulus(a, b):
     if b == 0:
         return "خطا: مقسوم‌علیه نمی‌تواند صفر باشد!"
